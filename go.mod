e6abf5374e53e81acad6e9f9f7f7a8845651f5a3
N-Silbernagel <6422477+n-silbernagel@users.noreply.github.com>
2025-07-17 17:42:11 +0200

    Add basic file listening and clipboard writing

---
+++ go.mod
@@ -0,0 +1,15 @@
+module nsilbernagel/fs-clip
+
+go 1.24.1
+
+require (
+	github.com/fsnotify/fsnotify v1.9.0
+	golang.design/x/clipboard v0.7.1
+)
+
+require (
+	golang.org/x/exp/shiny v0.0.0-20250711185948-6ae5c78190dc // indirect
+	golang.org/x/image v0.29.0 // indirect
+	golang.org/x/mobile v0.0.0-20250711185624-d5bb5ecc55c0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"errors"
+	"github.com/fsnotify/fsnotify"
+	"golang.design/x/clipboard"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func ensureDir(dirName string) error {
+	err := os.Mkdir(dirName, 0777)
+	if err == nil {
+		return nil
+	}
+	if !os.IsExist(err) {
+		return err
+	}
+
+	// check that the existing path is a directory
+	info, err := os.Stat(dirName)
+	if err != nil {
+		return err
+	}
+	if !info.IsDir() {
+		return errors.New("path exists but is not a directory")
+	}
+
+	return nil
+}
+
+func addFileToClipboard(filePath string) error {
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	contentType := http.DetectContentType(fileContent)
+
+	log.Println("contentType:", contentType)
+
+	if strings.HasPrefix(contentType, "text/") ||
+		strings.HasPrefix(contentType, "application/vnd.*") ||
+		contentType == "application/rtf" ||
+		contentType == "application/json" ||
+		contentType == "application/xml" ||
+		contentType == "application/pdf" {
+		log.Println("writing text")
+		clipboard.Write(clipboard.FmtText, fileContent)
+		return nil
+	}
+
+	if strings.HasPrefix(contentType, "image/") {
+		clipboard.Write(clipboard.FmtImage, fileContent)
+		return nil
+	}
+
+	return errors.New("unsupported fileContent type:" + contentType)
+}
+
+func main() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	watchPath := homeDir + string(os.PathSeparator) + "fs-clip-watch"
+
+	err = ensureDir(watchPath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create new watcher.
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		panic(err)
+	}
+	defer watcher.Close()
+
+	err = clipboard.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	go watchEvents(watcher)
+
+	err = watcher.Add(watchPath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Block main goroutine forever.
+	<-make(chan struct{})
+}
+
+func watchEvents(watcher *fsnotify.Watcher) {
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+
+			if event.Has(fsnotify.Create) {
+				handleFileCreate(event)
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			log.Println("File watcher error:", err)
+		}
+	}
+}
+
+func handleFileCreate(event fsnotify.Event) {
+	err := addFileToClipboard(event.Name)
+	if err != nil {
+		// fail silently, shouldn't interrupt the program
+		log.Println("error while adding file to clipboard:", err)
+		return
+	}
+
+	err = os.Remove(event.Name)
+	if err != nil {
+		// fail silently, shouldn't interrupt the program
+		log.Println("error while removing file:", err)
+	}
+}
